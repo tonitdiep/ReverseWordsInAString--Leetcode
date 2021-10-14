af33427c17cc0020996434e629776c89d3503d18
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-14 23:46:24 +0000

    Leetcode 557 questions, example(s), and constraints

---
+++ index.js
@@ -0,0 +1,21 @@
+Leetcode 557 Reverse Words in a String III
+
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+//reverse the order of characters in each word
+//preserve whitespace and initial word order
+var reverseWords = function(s) {
+
+  
+}
